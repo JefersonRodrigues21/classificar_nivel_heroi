3a6e81f74fd8f8933ba3bbd0209c6bec834fa304
JefersonRodrigues21 <dev.geekjs@gmail.com>
2024-10-28 23:28:50 -0300

    desafio classificador nível herói - pronto

---
+++ index.js
@@ -0,0 +1,12 @@
+let heroi = "herói-1"
+let xp = 1000
+
+if (xp >= 0 && xp <= 1000) {
+    console.log("Ferro");
+} else if (xp >= 1001 && xp <= 2000) {
+    console.log("Bronze");
+} else if (xp >= 2001 && xp <= 5000) {
+    console.log("Prata");
+} else if (xp >= 5001 && xp < 7000) {
+    console.log("Ouro");
+} else if()
